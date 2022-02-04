28c8c62a9cc584872d72ca8f240e43c020f32c70
KamilaStuglik <77364670+KamilaStuglik@users.noreply.github.com>
2022-02-04 23:09:02 +0100

    Added forecast feature (with fake data) to JS
    HTML div block removed from index.html and added to JS string to concatenate the data.

--- src/app.js
+++ src/app.js
@@ -13,6 +13,37 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast (){
+    let forecastElement = document.querySelector ("#forecast");
+
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+    
+    let forecastHTML = `<div class="row">`;
+    days.forEach(function (day) {
+    forecastHTML = forecastHTML + `
+        
+                        <div class="col-2">
+                            <div class="weather-forecast-date">
+                    ${day}
+                        </div>
+                    <img src="https://openweathermap.org/img/wn/03d@2x.png" alt="" width="42">
+                    <div class="weather-forecast-temperatures">
+                    <span class="weather-temperature-max">
+                        18°
+                    </span>
+                    <span class="weather-temperature-min"> 
+                        12°
+                    </span> 
+                    </div>
+                        </div>
+        
+    `;
+    });
+
+    forecastHTML = forecastHTML + `</div>`;
+    forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response){
     let temperatureElement = document.querySelector("#temp");
     let cityElement = document.querySelector("#city");
@@ -76,3 +107,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", showCelsiusTemp);
 
 search("Sydney");
+displayForecast ();
